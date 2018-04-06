0683598551366bed1f4b45220eccb985be78e7bd
Ori Roth <ori.rothh@gmail.com>
2018-04-07 02:04:01 +0300

    sync

--- src/test/java/bashful/example/application/Main.java
+++ src/test/java/bashful/example/application/Main.java
@@ -3,7 +3,7 @@ package bashful.example.application;
 import bashful.Bashing;
 import bashful.annotations.Bash;
 
-@Bash(value = "echo \"a\"", name = "Bashing")
+@Bash(value = "cmd.exe /c echo \"a\"", name = "Bashing")
 public class Main {
 	public static void main(String[] args) {
 		System.out.println(Bashing.output);
